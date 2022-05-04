2033f47cc50f0a689d4e3ad4443f05a3b8306e07
Shynal-Nazar <87614426+Shynal-Nazar@users.noreply.github.com>
2022-05-04 21:39:22 +0300

    add js

--- js/button.js
+++ js/button.js
@@ -1,5 +1,9 @@
 const btnNext = document.querySelector(".button");
+const mainContent = document.querySelector(".main-content");
 const form = document.querySelector(".form");
+const formProgres = document.querySelector(".form-progres");
+const formQuestion = document.querySelector(".form-title");
+const formAnsver = document.querySelector(".ansver-content");
 const lable1 = document.querySelector("#women");
 const lable2 = document.querySelector("#men");
 
@@ -20,15 +24,29 @@ function ChangeClassBtn(evt) {
   }
 }
 
-btnNext.addEventListener("click", () => {
-  console.log("klick");
-});
+// btnNext.addEventListener("click", () => {
+//   console.log("klick");
+// });
 
 form.addEventListener("submit", onformSabmit);
 
 function onformSabmit(event) {
-  const formElements = event.currentTarget.elements;
-
-  console.log(formElements.sex.value);
   event.preventDefault();
+  uncheckBoxes();
+  btnNext.classList.add("visually-hidden");
+  formProgres.classList.remove("visually-hidden");
+  const formElements = event.currentTarget.elements;
+  mainContent.classList.add("visually-hidden");
+  formQuestion.textContent =
+    "В какое время суток Вы чувствуете себя наиболее комфортно ? ";
+
+  formAnsver.classList.remove("main-ansver-row");
+  formAnsver.classList.add("main-ansver-column");
+  console.log(formQuestion.textContent);
+  console.log(formElements.sex.value);
+}
+
+function uncheckBoxes() {
+  lable1.checked = false;
+  lable2.checked = false;
 }
